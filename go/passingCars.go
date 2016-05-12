0afb168191121a06521fbb9e81d75722ab537852
horacio23 <horacio.delgado23@gmail.com>
2016-05-12 15:13:57 -0400

    added comments explaining the passingCar solution

--- go/passingCars.go
+++ go/passingCars.go
@@ -12,13 +12,16 @@ func Solution(A []int) int {
 	count := 0
 	mult := 0
 	start := false
+
 	for _, v := range A {
 		if v == 0 {
+			// every time a zero is found, we count each 1 as many times as we have encountered zeros
 			mult++
 			start = true
 		}
 
 		if start && v == 1 {
+			// add the current count to the multiplication of all the zeroes we have found
 			count = count + (1 * mult)
 
 			if count > 1000000000 {
